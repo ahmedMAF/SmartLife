12386eace97211ad46ae7b2665328e2bfe4eed2c
ahmed <ahmed6mohmad6ahmed@gmail.com>
2025-05-10 16:57:24 +0300

    localizable db first itiration

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -6,12 +6,14 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string NameAr { get; set; }
     public string Image { get; set; }
     public string Description { get; set; }
+    public string DescriptionAr { get; set; }
     public string Category { get; set; }
+    public string CategoryAr { get; set; }
     public List<SubModule> Features { get; set; } = [];
     public List<SubModule> Models { get; set; } = [];
     public List<GalleryEntry> Photos { get; set; } = [];
     public List<string> Videos { get; set; } = [];
-
 }
