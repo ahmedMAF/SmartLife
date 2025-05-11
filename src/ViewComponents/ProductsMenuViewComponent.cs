7dc6d98035e3181dbe88a38ec9f9165c7230e731
Adham084-Phone <adham.abo.rabie@gmail.com>
2025-05-11 20:59:42 +0300

    NOT TESTED

--- src/ViewComponents/ProductsMenuViewComponent.cs
+++ src/ViewComponents/ProductsMenuViewComponent.cs
@@ -8,10 +8,37 @@ namespace SmartLife.ViewComponents;
 [ViewComponent(Name = "ProductsMenu")]
 public class ProductsMenuViewComponent(SmartLifeDb context) : ViewComponent
 {
+    public bool IsAr { get; set; } = StringComparer.OrdinalIgnoreCase.Equals(System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName, "ar");
+    
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        List<string> categories = await context.Products.Select(p => p.Category).Distinct().ToListAsync();
-        var products = new Dictionary<string, IList<Product>>
+        List<string> categories;
+        Dictionary<string, IList<Product>> products;
+        
+        if (IsAr)
+        {
+         categories = await context.Products.Select(p => p.CategoryAr).Distinct().ToListAsync();
+         products = new Dictionary<string, IList<Product>>
+        {
+            ["-"] = await context.Products
+                    .Where(p => p.CategoryAr == "")
+                    .ToListAsync()
+        };
+                    
+        foreach (var category in categories)
+        {
+            if (category == "")
+                continue;
+
+            products[category] = await context.Products
+                .Where(p => p.CategoryAr == category)
+                .ToListAsync();
+        }
+        }
+        else
+        {
+            categories = await context.Products.Select(p => p.Category).Distinct().ToListAsync();
+        products = new Dictionary<string, IList<Product>>
         {
             ["-"] = await context.Products
                     .Where(p => p.Category == "")
@@ -26,6 +53,7 @@ public class ProductsMenuViewComponent(SmartLifeDb context) : ViewComponent
             products[category] = await context.Products
                 .Where(p => p.Category == category)
                 .ToListAsync();
+        }
         }
 
         return View("Index", new ProductsMenu
