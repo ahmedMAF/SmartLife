5cc63be11e79b48e65d604678bff7769569a61ef
AhmedMAfana <ahmed6mohmad6ahmed@gmail.com>
2026-02-07 14:30:09 +0200

    the final commit

--- src/Pages/Products/Index.cshtml.cs
+++ src/Pages/Products/Index.cshtml.cs
@@ -1,23 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using SmartLife.Models;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace SmartLife.Pages.Products;
 
 public class IndexModel(SmartLifeDb context, IStringLocalizer<IndexModel> localizer) : PageModel
 {
-    public IList<Product> Products { get; set; } = default!;
+    public IPagedList<Product> Products { get; set; } = default!;
     public bool IsAr { get; set; } = StringComparer.OrdinalIgnoreCase.Equals(System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName, "ar");
     public IStringLocalizer<IndexModel> Localizer { get; } = localizer;
 
-    public async Task<IActionResult> OnGetAsync(string? id)
+    public string? CurrentCategory { get; set; }
+
+    public IActionResult OnGet(string? id, [FromQuery] int? page)
     {
-        Products = await context.Products
+        int pageNum = page ?? 1;
+
+        IOrderedQueryable<Product> query = context.Products
             .Where(p => id == null || (IsAr ? p.CategoryAr : p.Category) == id)
-            .OrderBy(p => p.OrderIndex)
-            .ToListAsync();
+            .OrderBy(p => p.OrderIndex);
+
+        Products = query.ToPagedList(pageNum, 10);
+        CurrentCategory = id;
 
         return Page();
     }
